329fcfb907c2a0d990e1076186932823d43262c7
manomarichal <mano.marichal@gmail.com>
2025-03-08 10:35:54 +0100

    attacking

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,10 +5,18 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private float _detectionRange = 10f;
     [SerializeField] private LayerMask _obstacleMask;
+
+    [Header("Attack settings")]
+    [SerializeField] private Vector3 attackBoxSize = new Vector3(2f, 2f, 2f); 
+    [SerializeField] private Vector3 attackBoxOffset = new Vector3(0f, 0f, 1f); 
+    [SerializeField] private int attackDamage = 10;
+    [SerializeField] private LayerMask attackableLayers; 
+    [SerializeField] private float attackCooldown = 1f;
+
     private bool _isFollowing = false;
     private Transform _player;
-
     private NavMeshAgent _navMeshAgent;
+    private bool canAttack = true;
 
     void Start()
     {
@@ -40,6 +48,10 @@ public class EnemyController : MonoBehaviour
         
         if (_isFollowing)
         {
+            if (canAttack && CheckIfTargetInRange())
+            {
+                Attack();
+            }
             FollowPlayer();
         }
     }
@@ -64,5 +76,46 @@ public class EnemyController : MonoBehaviour
             return hit.collider.CompareTag("Player");
         }
         return true;
+    }
+
+    private Collider[] GetTargetsInRange()
+    {
+        Vector3 attackPosition = transform.position + transform.TransformDirection(attackBoxOffset);
+        Collider[] hitObjects = Physics.OverlapBox(attackPosition, attackBoxSize * 0.5f, transform.rotation, attackableLayers);
+
+        return hitObjects;
+    }
+    private bool CheckIfTargetInRange()
+    {
+        return GetTargetsInRange().Length>0;
+    }
+    
+    public void Attack()
+    {        
+        Debug.Log("Attack!");
+
+        canAttack = false;
+        Invoke(nameof(ResetAttack), attackCooldown); 
+        
+        Collider[] hitObjects = GetTargetsInRange();
+        foreach (Collider hit in hitObjects)
+        {
+            return;
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        canAttack = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw attack box in editor for visualization
+        Gizmos.color = Color.red;
+        Vector3 attackPosition = transform.position + transform.TransformDirection(attackBoxOffset);
+        Gizmos.matrix = Matrix4x4.TRS(attackPosition, transform.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, attackBoxSize);
     }
 }
